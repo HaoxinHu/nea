735c23ac662f501c7121dae6fa0e1de5f3cc2706
Sanli <huhozin@gmail.com>
2026-03-13 14:56:34 +0000

    Implement signup functionality and enhance login process

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        f = open("login.txt", "w")
+        f.write(request.form.get('username') + ":" + request.form.get('pw'))
+        f.close()
+    return render_template('signup.html')
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        pw = request.form.get('pw')
-        if pw == "password":
-            return "Access granted"
-        else:   
-            return "Access denied"
+        pw = request.form['password']
+        f = open("login.txt", "r")
+        stored_data = f.read().split(":")
+        f.close()
+        if request.form.get('username') == stored_data[0] and request.form.get('pw') == stored_data[1]:
+            return "Logged in!"
     return render_template('index.html')
