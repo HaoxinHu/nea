01e7b0752075d60072c910d995d86d0d73a26e72
Sanli <huhozin@gmail.com>
2026-03-16 11:26:27 +0000

    Refactor signup and home routes to use SQLite for user data storage and authentication

--- app.py
+++ app.py
@@ -3,24 +3,33 @@ import sqlite3
 
 app = Flask(__name__)
 
+DB_NAME = 'database.db'
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        f = open("login.txt", "w")
-        f.write(request.form.get('username') + ":" + request.form.get('pw'))
-        f.close()
+        con = sqlite3.connect(DB_NAME)
+        cur = con.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)",
+        (request.form.get('username'), request.form.get('pw')))
+        con.commit()
+        con.close()
     return render_template('signup.html')
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        con = sqlite3.connect('users.db')
+        con = sqlite3.connect(DB_NAME)
         cur = con.cursor()
         cur.execute("SELECT * FROM users WHERE username=? AND password=?",
-        (request.form['username'], request.form['password']))
+        (request.form['username'], request.form['pw']))
         result = cur.fetchone()
+        con.close()
         if result:
             return "Access granted"
         else:
             return "Access denied"
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
